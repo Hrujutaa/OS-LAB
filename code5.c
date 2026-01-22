bdbe8bcd32fa6ebd931db70b00919d166c9781f4
Hrujutaa <hruju05@gmail.com>
2026-01-22 11:03:29 +0530

    Add files via upload

---
+++ code5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+	int day;
+	printf("enter day number(1-7): ");
+	scanf("%d",&day);
+
+	switch(day){ 
+		case 1:
+			printf("Monday");
+			break;
+		case 2:
+			printf("Tuesday");
+
+
+
+
+			break;	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
